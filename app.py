9b65012279c24244df56167b7dead074586c4cf6
Graham Ross <grahamross99@gmail.com>
2021-03-12 15:23:40 +0000

    Added a working carousel

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 
-from flask import Flask, flash, redirect, render_template, request, session, jsonify
+from flask import Flask, render_template
 from flask_session import Session
 
 # Configure application
 app = Flask(__name__)
-
-# Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 Session(app)
 
 
