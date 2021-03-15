2a40df4bec4599ade1b626dd3aaaa080040d32b4
Graham Ross <grahamross99@gmail.com>
2021-03-15 22:55:03 +0000

    Improved home screen and almost finished save function

--- app.py
+++ app.py
@@ -33,29 +33,60 @@ def after_request(response):
     return response
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def index():
-	top_headlines = newsapi.get_top_headlines(language='en')
+    
+    categories = ['Politics', 'Finance', 'Technology', 'Sport', 'World']
 
-	sport = newsapi.get_everything(language='en',
-									  q='sport',
-									  domains = 'cnn.com',
-                                      sort_by='publishedAt',
-                                      page=1)
-	return render_template('index.html', headlines=top_headlines, sport=sport)
+    try:
+
+        top_headlines = newsapi.get_top_headlines(language='en', country='gb', page_size=3)
+        article_dicts = []
+        for category in categories:
+            articles = newsapi.get_everything(language='en',
+                                           q=category,
+                                           domains = 'bbc.co.uk',
+                                           sort_by='publishedAt',
+                                           page_size=3,
+                                           page=1)
+            article_dict = {'category': category, 'articles': articles['articles']}
+            article_dicts.append(article_dict)
+
+    except Exception as e: 
+            print(e)
+            top_headlines = {'articles': [
+                                        {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                        {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                        {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                        ]}
+            article_dicts = []
+            for category in categories:
+                articles = [{'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                            {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                            {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'}]
+                article_dict = {'category': category, 'articles': articles}    
+                article_dicts.append(article_dict)
+
+            flash("Could not load news from NewsAPI")
+    return render_template('index.html', headlines=top_headlines, all_articles=article_dicts)
 
 
-@app.route("/save", methods=['POST'])
-def save():
+@app.route("/saving", methods=['POST'])
+def saving():
     """Add an article to a user's save list."""
     title = request.form['title']
+
     conn = sqlite3.connect(db)
     c = conn.cursor()
     with conn:
         # Check if the article is in the articles table
         c.execute("SELECT * FROM articles WHERE title = :title", {'title': title})
         if len(c.fetchall()) == 0:
-        	c.execute("INSERT INTO articles (title) VALUES (:title)", {'title': title})
+            url = request.form['url']
+            image_url = request.form['image_url']
+            description = request.form['description']
+            c.execute("INSERT INTO articles (title, url, image_url, description) VALUES (:title, :url, :image_url, :description)", 
+                     {'title': title, 'url': url, 'image_url': image_url, 'description': description})
 
         # Obtain the id of the article from the articles table
         c.execute("SELECT id FROM articles WHERE title = :title", {'title': title})
@@ -69,19 +100,166 @@ def save():
         else:
             # Remove the article from the user_articles table
             c.execute("DELETE FROM user_articles WHERE user_id = :user_id AND article_id = :article_id", {'user_id': session['user_id'], 'article_id': article_id})
-    return redirect("/")
+    return redirect('/')
 
+
+@app.route("/saved")
+@login_required
+def saved():
+    """List a user's saved articles from the user_articles table"""
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    with conn:
+        # Find the articles that the user has saved
+        c.execute("""SELECT * FROM articles WHERE id IN (
+                     SELECT article_id FROM user_articles WHERE user_id = :user_id)""", 
+                     {'user_id': session['user_id']})
+        rows = c.fetchall()
+        if len(rows) == 0:
+            return render_template('empty.html', title='Saved Articles', message="You have no saved articles")
+
+        # Transfer the article info into a dictionary
+        articles = {'articles': []}
+        saved_titles = []
+        for row in rows:
+            article_dict = {}
+            article_dict['title'] = row[1]
+            article_dict['url'] = row[2]
+            article_dict['urlToImage'] = row[3]
+            article_dict['description'] = row[4]
+            articles['articles'].append(article_dict)
+            saved_titles.append(row[1])
+        
+        return render_template('results.html', title='Saved Articles', articles=articles, saved_titles=saved_titles)
 
 
 @app.route("/sport")
 def sport():
-	"""Display a list of sport articles"""
-	sport = newsapi.get_everything(language='en',
-								  q='sport',
-								  domains = 'cnn.com',
-                                  sort_by='publishedAt',
-                                  page=1)
-	return render_template('results.html', sport=sport)
+    """Display a list of sport articles"""
+    # Obtain a list of all the user's saved articles
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    with conn:
+        # Find the articles that the user has saved
+        c.execute("""SELECT * FROM articles WHERE id IN (
+                     SELECT article_id FROM user_articles WHERE user_id = :user_id)""", 
+                     {'user_id': session['user_id']})
+        rows = c.fetchall()
+        saved_titles = []
+        for row in rows:
+            saved_titles.append(row[1])
+
+    try:
+        sport = newsapi.get_everything(language='en',
+                                       q='sport',
+                                       domains = 'bbc.co.uk',
+                                       sort_by='publishedAt',
+                                       page=1)
+    except:
+        sport = {'articles': [
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    ]}
+        flash("Could not load news from NewsAPI")
+    print(saved_titles)
+    return render_template('results.html', title="Sport", articles=sport, saved_titles=saved_titles)
+
+
+@app.route("/technology")
+def technology():
+    """Display a list of technology articles"""
+    try:
+        technology = newsapi.get_everything(language='en',
+                                       q='technology',
+                                       domains = 'bbc.co.uk',
+                                       sort_by='publishedAt',
+                                       page=1)
+    except:
+        technology = {'articles': [
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    ]}
+        flash("Could not load news from NewsAPI")
+    return render_template('results.html', title="Technology", articles=technology, saved_titles=saved_titles)
+
+
+@app.route("/politics")
+def politics():
+    """Display a list of politics articles"""
+    try:
+        politics = newsapi.get_everything(language='en',
+                                       q='politics',
+                                       domains = 'bbc.co.uk',
+                                       sort_by='publishedAt',
+                                       page=1)
+    except:
+        politics = {'articles': [
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    ]}
+        flash("Could not load news from NewsAPI")
+    return render_template('results.html', title="Politics", articles=politics, saved_titles=saved_titles)
+
+
+@app.route("/finance")
+def finance():
+    """Display a list of finance articles"""
+    try:
+        finance = newsapi.get_everything(language='en',
+                                       q='finance',
+                                       domains = 'bbc.co.uk',
+                                       sort_by='publishedAt',
+                                       page=1)
+    except:
+        finance = {'articles': [
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    ]}
+        flash("Could not load news from NewsAPI")
+    return render_template('results.html', title="Finance", articles=finance, saved_titles=saved_titles)
+
+
+@app.route("/world")
+def world():
+    """Display a list of world articles"""
+    try:
+        world = newsapi.get_everything(language='en',
+                                       q='world',
+                                       domains = 'bbc.co.uk',
+                                       sort_by='publishedAt',
+                                       page=1)
+    except:
+        world = {'articles': [
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    ]}
+        flash("Could not load news from NewsAPI")
+    return render_template('results.html', title="World", articles=world, saved_titles=saved_titles)
+
+
+@app.route("/search/<query>")
+def search(query):
+    """Search for any category"""
+    try:
+        search = newsapi.get_everything(language='en',
+                                       q=query,
+                                       domains = 'bbc.co.uk',
+                                       sort_by='publishedAt',
+                                       page=1)
+    except:
+        search = {'articles': [
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    {'title': 'Error', 'description': 'Error', 'urlToImage': 'None'},
+                                    ]}
+        flash("Could not load news from NewsAPI")
+    return render_template('results.html', title="Search", articles=search, saved_titles=saved_titles)
+
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -197,10 +375,4 @@ def register():
 
 
 if __name__ == '__main__':
-	news = newsapi.get_everything(language='en',
-								  q="Turn your living room couch into the best seats at every game with Samsung's 2021 TVs",
-								  domains = 'cnn.com',
-                                  sort_by='publishedAt',
-                                  page=1)
-	print(news)
 	app.run(debug=True)
--- static/js/results.js
+++ static/js/results.js
@@ -1,22 +1,32 @@
-// $(document).ready(function(){
-//     $(".save-button").submit(function() {
-//         e.preventDefault();
-//         document.write('hello');
-//         $.ajax({
-
-//             type: 'POST',
-//             url: '/save',
-//         })
-//         return false;
-//     });
-// });
-
-function saveArticle(title, elem) {
-    $.ajax({
-            type: 'POST',
-            url: '/save',
-            data: {'title': title}
-        })
+function saveArticle(title, url, image_url, description, elem, user_id) {
+	if (!user_id) {
+		let host = window.location.hostname
+    	window.location.href =  "/login";
+    	return
+    }
     elem.find(">:first-child").toggleClass('saved');
+
+        $.ajax({
+            type: 'POST',
+            url: '/saving',
+            data: {'title': title,
+                   'url': url,
+                   'image_url': image_url,
+                   'description': description}
+        });
     return
+}
+
+
+function check_results(saved_titles) {
+    let title_elems = $('.post-container').find('.article-title');
+    let button_elems = $('.post-container').find('.plus');
+    console.log(button_elems[1])
+    for (var i = 0; i < saved_titles.length; i++) {
+        for (let j = 0; j < title_elems.length; j++) {
+            if (saved_titles[i] === title_elems[j].text) {
+                console.log(j);
+            }
+        }
+    }
 }
